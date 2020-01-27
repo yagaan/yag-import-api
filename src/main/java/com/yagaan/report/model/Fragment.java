f10f98e30ba3cdb192d90aa72a0be6da0a7b5fa2
afloch <afloch@yagaan.com>
2020-01-27 15:43:58 +0100

    add metadata on issue and fragments

--- src/main/java/com/yagaan/report/model/Fragment.java
+++ src/main/java/com/yagaan/report/model/Fragment.java
@@ -1,8 +1,11 @@
 package com.yagaan.report.model;
 
+import java.util.HashMap;
+import java.util.Map;
+
 /**
  * Source code location information.
- * 
+ *
  * @author antoine
  *
  */
@@ -14,48 +17,59 @@ public class Fragment {
 	private int ecolumn = -1;
 	private int length = -1;
 
+	private final Map<String, String> metadata;
+
 	/**
-	 * 
+	 *
 	 * @param file file path
 	 * @param line line number, 1 based.
 	 */
-	public Fragment(String file, int line) {
+	public Fragment(final String file, final int line) {
 		super();
 		this.file = file;
 		this.line = line;
+		this.metadata = new HashMap<>();
+	}
+
+	public Map<String, String> getMetadata() {
+		return this.metadata;
+	}
+	public Fragment metadata(String key, String value) {
+		this.metadata.put(key, value);
+		return this;
 	}
 
 	/**
-	 * 
+	 *
 	 * @param column column number, 0-based
 	 * @return the current fragment
 	 */
-	public Fragment column(int column) {
+	public Fragment column(final int column) {
 		this.column = column;
 		return this;
 	}
 
-	public Fragment length(int length) {
+	public Fragment length(final int length) {
 		this.length = length;
 		return this;
 	}
 
 	/**
-	 * 
+	 *
 	 * @param col a column number, 0-based
 	 * @return then current fragment
 	 */
-	public Fragment endColumn(int col) {
+	public Fragment endColumn(final int col) {
 		this.ecolumn = col;
 		return this;
 	}
 
 	/**
-	 * 
+	 *
 	 * @param line a line number, 1-based
 	 * @return then current fragment
 	 */
-	public Fragment endLine(int line) {
+	public Fragment endLine(final int line) {
 		this.eline = line;
 		return this;
 	}
@@ -64,98 +78,98 @@ public class Fragment {
 	 * @return the last column of the selection, 0-based, or -1 if undefined
 	 */
 	public int getEndColumn() {
-		return ecolumn;
+		return this.ecolumn;
 	}
 
 	/**
 	 * @return the last line of the selection, 1-based, or -1 if undefined
 	 */
 	public int getEndLine() {
-		return eline;
+		return this.eline;
 	}
 
 	public boolean hasColumn() {
-		return column != -1;
+		return this.column != -1;
 	}
 
 	public boolean hasLength() {
-		return length != -1;
+		return this.length != -1;
 	}
 
 	/**
 	 * Get the source code file relative path of the fragment.
-	 * 
+	 *
 	 * @return
 	 */
 	public String getFile() {
-		return file;
+		return this.file;
 	}
 
 	/**
 	 * Set the source code file relative path of the fragment.
-	 * 
+	 *
 	 * @param file
 	 */
-	public void setFile(String file) {
+	public void setFile(final String file) {
 		this.file = file;
 	}
 
 	/**
 	 * Get the start line of the code fragment, 1-based.
-	 * 
+	 *
 	 * @return
 	 */
 	public int getLine() {
-		return line;
+		return this.line;
 	}
 
 	/**
 	 * Set the start line of the code fragment, 1-based.
-	 * 
+	 *
 	 * @param line
 	 */
-	public void setLine(int line) {
+	public void setLine(final int line) {
 		this.line = line;
 	}
 
 	/**
 	 * Get the start column of the code fragment, 0-based.
-	 * 
+	 *
 	 * @return
 	 */
 	public int getColumn() {
-		return column;
+		return this.column;
 	}
 
 	/**
 	 * Set the start column of the code fragment, 0-based.
-	 * 
+	 *
 	 * @param column
 	 */
-	public void setColumn(int column) {
+	public void setColumn(final int column) {
 		this.column = column;
 	}
 
 	/**
 	 * Get the length of the code fragment, or -1 if undefined.
-	 * 
+	 *
 	 * @return
 	 */
 	public int getLength() {
-		return length;
+		return this.length;
 	}
 
 	/**
 	 * Set the length of the code fragment.
-	 * 
+	 *
 	 * @param length
 	 */
-	public void setLength(int length) {
+	public void setLength(final int length) {
 		this.length = length;
 	}
 
 	@Override
 	public String toString() {
-		return file + " at l." + line;
+		return this.file + " at l." + this.line;
 	}
 }
--- src/main/java/com/yagaan/report/model/Issue.java
+++ src/main/java/com/yagaan/report/model/Issue.java
@@ -1,5 +1,7 @@
 package com.yagaan.report.model;
 
+import java.util.HashMap;
+import java.util.Map;
 import java.util.Optional;
 
 public class Issue {
@@ -11,127 +13,140 @@ public class Issue {
 
 	private Path path;
 
-	public Issue(String name, Fragment location) {
+	private final Map<String, String> metadata;
+
+	public Issue(final String name, final Fragment location) {
 		this.name = name;
 		this.location = location;
+		this.metadata = new HashMap<>();
+	}
+
+	public Map<String, String> getMetadata() {
+		return this.metadata;
 	}
 
 	/**
 	 * Get an optional message about the issue explanation
-	 * 
+	 *
 	 * @return
 	 */
 	public String getMessage() {
-		return message;
+		return this.message;
 	}
 
 	/**
 	 * Set the explanation message of the issue
-	 * 
+	 *
 	 * @param message
 	 */
-	public void setMessage(String message) {
+	public void setMessage(final String message) {
 		this.message = message;
 	}
 
 	/**
 	 * Set the explanation message of the issue
-	 * 
+	 *
 	 * @param message
 	 */
-	public Issue message(String message) {
+	public Issue message(final String message) {
 		this.message = message;
 		return this;
 	}
 
 	/**
 	 * Get the name of the issue checker
-	 * 
+	 *
 	 * @return
 	 */
 	public String getName() {
-		return name;
+		return this.name;
 	}
 
 	/**
 	 * Get the location of the vulnerable code
-	 * 
+	 *
 	 * @return
 	 */
 	public Fragment getLocation() {
-		return location;
+		return this.location;
 	}
 
-	public Issue location(Fragment location) {
+
+	public Issue metadata(String key, String value) {
+		this.metadata.put(key, value);
+		return this;
+	}
+
+	public Issue location(final Fragment location) {
 		this.location = location;
 		return this;
 	}
 
-	public void setName(String name) {
+	public void setName(final String name) {
 		this.name = name;
 	}
 
-	public void setLocation(Fragment location) {
+	public void setLocation(final Fragment location) {
 		this.location = location;
 	}
 
-	public void setStatus(Status status) {
+	public void setStatus(final Status status) {
 		this.status = status;
 	}
 
-	public Issue status(Status status) {
+	public Issue status(final Status status) {
 		this.status = status;
 		return this;
 	}
 
-	public void setLanguage(String language) {
+	public void setLanguage(final String language) {
 		this.language = language;
 	}
 
-	public Issue language(String language) {
+	public Issue language(final String language) {
 		this.language = language;
 		return this;
 	}
 
-	public Issue path(Path propagationPath) {
+	public Issue path(final Path propagationPath) {
 		this.path = propagationPath;
 		return this;
 	}
 
-	public void setPath(Path path) {
+	public void setPath(final Path path) {
 		this.path = path;
 	}
 
 	/**
 	 * Get the review status of the issue.
-	 * 
+	 *
 	 * @return
 	 */
 	public Status getStatus() {
-		return status;
+		return this.status;
 	}
 
 	/**
 	 * Get the language of the vulnerable code.
-	 * 
+	 *
 	 * @return
 	 */
 	public String getLanguage() {
-		return language;
+		return this.language;
 	}
 
 	/**
 	 * Get the optional propagation path that cause the issue.
-	 * 
+	 *
 	 * @return
 	 */
 	public Optional<Path> getPath() {
-		return Optional.ofNullable(path);
+		return Optional.ofNullable(this.path);
 	}
 
 	@Override
 	public String toString() {
-		return name;
+		return this.name;
 	}
 
 }
